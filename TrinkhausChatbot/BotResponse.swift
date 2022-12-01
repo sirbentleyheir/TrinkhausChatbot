2c4fbf638b48e943d1b6944899618dfe2d64efbd
sirbentleyheir <mahabuddi.sadashiva@gmail.com>
2022-12-01 18:42:52 +0100

    Setup

---
+++ TrinkhausChatbot/BotResponse.swift
@@ -0,0 +1,8 @@
+//
+//  BotResponse.swift
+//  TrinkhausChatbot
+//
+//  Created by Bentley on 01.12.22.
+//
+
+import Foundation
--- TrinkhausChatbot/ContentView.swift
+++ TrinkhausChatbot/ContentView.swift
@@ -8,9 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var messageText = ""
+    @State var messages: [String] = ["Welcome to Chatbot 2.0"]
+    
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            HStack {
+                Text("Trinkhaus")
+                    .font(.largeTitle)
+                    .bold()
+                
+                Image(systemName: "bubble.left.fill")
+                    .font(.system(size: 26))
+                    .foregroundColor(Color.blue)
+            }
+            
+            ScrollView {
+                // Messages
+            }
+            
+            HStack {
+                TextField("Type something", text: $messageText)
+                    .padding()
+                    .background(Color.gray.opacity(0.1))
+                    .cornerRadius(10)
+                    .onSubmit {
+                        // Send Message
+                    }
+                
+                Button {
+                    // Send Message
+                } label: {
+                    Image(systemName: "paperplane.fill")
+                }
+                .font(.system(size: 26))
+                .padding(.horizontal, 10)
+            }
             .padding()
+        }
     }
 }
 
