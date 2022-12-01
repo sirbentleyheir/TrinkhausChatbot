07ea89749290b91a7d04424756b7203b06f230d5
sirbentleyheir <mahabuddi.sadashiva@gmail.com>
2022-12-01 17:44:14 +0100

    Initial Commit

---
+++ TrinkhausChatbot/TrinkhausChatbotApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrinkhausChatbotApp.swift
+//  TrinkhausChatbot
+//
+//  Created by Bentley on 01.12.22.
+//
+
+import SwiftUI
+
+@main
+struct TrinkhausChatbotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
